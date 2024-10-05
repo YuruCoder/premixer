95894795cf4eecb3d6fccff04708f84673e773f6
Hojun Kim <76956067+YuruCoder@users.noreply.github.com>
2024-10-05 15:00:40 +0900

    support android

--- lib/interfaces/database_interface.dart
+++ lib/interfaces/database_interface.dart
@@ -11,8 +11,11 @@ abstract class DatabaseInterface {
     required String name,
     String description = '',
   });
+
   List<SourceModel> getAllSources();
+
   SourceModel getSourceById(int id);
+
   bool deleteSourceById(int id);
 
   void setRecipe({
@@ -22,15 +25,21 @@ abstract class DatabaseInterface {
     required int ratioA,
     required int ratioB,
   });
+
   List<RecipeModel> getAllRecipes();
+
   RecipeModel getRecipeById(int id);
+
   bool deleteRecipeById(int id);
 
   void setPreset({
     required String name,
     required List<RecipeModel> recipeList,
   });
+
   List<PresetModel> getAllPresets();
+
   PresetModel getPresetById(int id);
+
   bool deletePresetById(int id);
 }
